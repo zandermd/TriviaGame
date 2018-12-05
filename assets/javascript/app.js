9072f3c04fefca3b2445c7d045ea7dd92d55ebf1
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2018-12-04 17:22:56 -0700

    additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//get document ready to run JS. create empty strings for answers
 $(document).ready(function () {
     var types = ""
     var shatter = ""
@@ -6,11 +7,11 @@ $(document).ready(function () {
     var correctAnswers = 0;
     var inCorrectAnswers = 0;
     var unAnswered = 0;
-    var resultcalculator = function () {
+    var resultCalculator = function () {
     }
     // console.log('ready');
 
-
+    // conditional logic for user answers 
     if (types === "All") {
         correctAnswers++
     } else if (types === "") {
@@ -18,6 +19,8 @@ $(document).ready(function () {
     } else {
         inCorrectAnswers++
     };
+    console.log(types);
+    // console.log('inCorrect');
 
     if (shatter === "A term for cannabis concentrate") {
         correctAnswers++
@@ -26,6 +29,7 @@ $(document).ready(function () {
     } else {
         inCorrectAnswers++
     };
+    console.log(shatter);
 
     if (science === "Cannabis Satvia") {
         correctAnswers++
@@ -34,6 +38,8 @@ $(document).ready(function () {
     } else {
         inCorrectAnswers++
     };
+    console.log(science);
+
 
     if (lethal === "None") {
         correctAnswers++
@@ -41,14 +47,38 @@ $(document).ready(function () {
         unAnswered++
     } else {
         inCorrectAnswers++
+    };
+    console.log(lethal);
 
-        var resultString = ""
-        resultString += " <h2> Correct Answers: " + correctAnswers + "</h2>"
-        resultString += " <h2> Incorrect Answers: " + inCorrectAnswers + "</h2>"
-        resultString += " <h2> Unnswered: " + unAnswered + "</h2>"
+    $("input[type=radio]").click(function () {
+        var formName = $(this).closest("form").attr('name')
+        if (formName == "types") {
+            location = $(this).val();
+            console.log(location);
+        } else if (formName == "shatter") {
+            toy = $(this).val();
+            console.log(toy);
+        } else if (formName == "science") {
+            itch = $(this).val();
+            console.log(itch);
+        } else if (formName == "none") {
+            holiday = $(this).val();
 
-        $("body").replaceWith(resultString);
+            console.log(science);
+            console.log(formName);
 
-    }
+        }
+
+        // set varible for time to count down
+        var count = 15
+        timer = setInterval(function () {
+            $("#counter").html(count--);
+            if (count == -1) {
+                clearInterval(timer);
+            };
+        }, 1500);
+
+    });
 });
+
 
